17868608ca29b172a5a8779a0f243118580c688e
Preet814 <113155100+Preet814@users.noreply.github.com>
2025-04-30 13:15:00 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"  
 }
 resource "aws_instance" "name" {
-    ami = "i-0eb64aa89b0b6fb4d"
+    ami = "ami-0e35ddab05955cf57"
     instance_type = "t2.micro"
     tags = {
       "Name" = "demo-instance" 
